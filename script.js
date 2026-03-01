821ba47d5afe1a1fdccdd3820f8d952e8ba02886
Gunel2727 <gunelemu@code.edu.az>
2026-03-01 14:28:40 +0400

    added

--- script.js
+++ script.js
@@ -3,27 +3,34 @@ let selectedBox = null;
 const boxes = document.querySelectorAll(".box");
 const areas = document.querySelectorAll(".area");
 
-
 boxes.forEach(function(box) {
   box.ondragstart = function() {
     selectedBox = this;
   };
 });
 
-
 areas.forEach(function(area) {
 
   area.ondragover = function(e) {
-    e.preventDefault(); 
+    e.preventDefault();
   };
 
   area.ondrop = function() {
 
     const number = parseInt(selectedBox.textContent);
-    const min = parseInt(this.dataset.min);
-    const max = parseInt(this.dataset.max);
+    let correctAreaId = "";
 
-    if (number >= min && number <= max) {
+    
+    if (number >= 1 && number <= 4) {
+      correctAreaId = "area1";
+    } else if (number >= 5 && number <= 8) {
+      correctAreaId = "area2";
+    } else if (number >= 9 && number <= 12) {
+      correctAreaId = "area3";
+    }
+
+    
+    if (this.id === correctAreaId) {
       this.appendChild(selectedBox);
     } else {
       alert("Yanlış yer!");
