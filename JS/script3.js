3d9db9a009d87b8abeaef6d5732d2f71bf702d4e
Gunel2727 <gunelemu@code.edu.az>
2026-03-13 23:55:42 +0400

    code added

--- JS/script3.js
+++ JS/script3.js
@@ -4,6 +4,27 @@ for (let i = 0; i < buttons.length; i++) {
     const button = buttons [i];
     button.addEventListener("click",function(ev){
         ev.preventDefault();
-       
+        let id=this.parentNode.getAttribute("data-id");
+        let str=this.previousElementSibling.previousElementSibling.innerText;
+        let basketStr=localStorage.getItem("basket");
+        let products={};
+        if(basketStr){
+            products=JSON.parse(basketStr);
+        }
+        
+        let existingProduct=products.find(p=>p.id==id);
+        if(existingProduct){
+            existingProduct.count++;
+        }else{
+            let product={
+                id,
+                price:+str.substring(str.indexOf(":")+1,str.indexOf("$")),
+                url:this.parentNode.previousElementSibling.getAttribute("src"),
+                desc:this.previousElementSibling.innerText,
+                name:this.parentNode.firstElementChild.innerText,
+                count:1 
+            }
+            products.push(product);
+        }
     })
 }
